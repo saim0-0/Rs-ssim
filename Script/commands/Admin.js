0a8770cbe60642f4eb1f1b1bee6355febd9af510
saim45544 <mstopq79@gmail.com>
2025-09-19 16:39:30 +0600

    Update Admin.js

--- Script/commands/Admin.js
+++ Script/commands/Admin.js
@@ -49,7 +49,7 @@ module.exports.run = async function({ api, event }) {
  attachment: fs.createReadStream(__dirname + "/cache/owner.jpg")
  }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/owner.jpg"));
 
- return request("https://i.imgur.com/vAaeGaG.jpeg")
+ return request("https://i.imgur.com/7prYCvy.jpeg")
  .pipe(fs.createWriteStream(__dirname + '/cache/owner.jpg'))
  .on('close', () => callback());
 };
